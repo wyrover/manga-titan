194aa8530c349a60783cec8c8b8cfa930768ac43
nothing628 <yogyphang@gmail.com>
2016-04-18 21:03:10 +0700

    Add Model and controller for rating

--- modules/Core/Entities/Rating.php
+++ modules/Core/Entities/Rating.php
@@ -1,10 +1,18 @@
 <?php
 namespace Modules\Core\Entities;
-   
+
 use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model {
 
-    protected $fillable = [];
-    protected $table = 'rating';
+	protected $fillable = [];
+	protected $table = 'rating';
+
+	public function manga() {
+		$this->belongsTo(__NAMESPACE__.'\Manga', 'id_manga');
+	}
+
+	public function users() {
+		$this->belongsTo(__NAMESPACE__.'\Users', 'id_users');
+	}
 }
